c249cd107df8319eb6ef10e74df060347f057988
sobhandip1 <sobhandip.samanta@zensar.com>
2020-08-13 09:13:04 +0530

    adding assignment on webform

---
+++ WebAppAssign11aug/WebAppAssign11aug/PlayerInfo.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PlayerInfo.aspx.cs" Inherits="WebAppAssign11aug.PlayerInfo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <asp:RadioButton ID="RadioButton1" runat="server" AutoPostBack="True" GroupName="b" OnCheckedChanged="RadioButton1_CheckedChanged" Text="CricketPlayers" />
+        <br />
+        <br />
+        <asp:RadioButton ID="RadioButton2" runat="server" AutoPostBack="True" GroupName="b" Text="TennisPlayers" OnCheckedChanged="RadioButton2_CheckedChanged" />
+        <br />
+        <br />
+        <asp:Panel ID="Panel1" runat="server">
+            <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/CricketPlayers.html">Virat Kohli</asp:HyperLink>
+            <br />
+            <br />
+            <asp:HyperLink ID="HyperLink2" runat="server" NavigateUrl="~/TennisPlayer.html">Sachin Tendulkar</asp:HyperLink>
+        </asp:Panel>
+        <asp:Panel ID="Panel2" runat="server">
+            <asp:HyperLink ID="HyperLink3" runat="server" NavigateUrl="~/Roger Federer.html">Roger Federer</asp:HyperLink>
+            <br />
+            <br />
+            <asp:HyperLink ID="HyperLink4" runat="server" NavigateUrl="~/Rafal Nadal.html">Rafal Nadal</asp:HyperLink>
+        </asp:Panel>
+    </form>
+    
+</body>
+</html>
---
+++ WindowsCalcuApp/WindowsCalcuApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorLib;
+namespace WindowsCalcuApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        CalcuClass cal;
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            cal = new CalcuClass();
+            cal.Num1 = Convert.ToInt32(txtNum1.Text);
+            cal.Num2 = Convert.ToInt32(txtNum2.Text);
+            lblResult.Text= "Addition of the two numbers:"+cal.Add().ToString();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            cal = new CalcuClass();
+            cal.Num1 = Convert.ToInt32(txtNum1.Text);
+            cal.Num2 = Convert.ToInt32(txtNum2.Text);
+            lblResult.Text = "Subtraction of the two numbers:" + cal.Sub().ToString();
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            cal = new CalcuClass();
+            cal.Num1 = Convert.ToInt32(txtNum1.Text);
+            cal.Num2 = Convert.ToInt32(txtNum2.Text);
+            lblResult.Text = "Multiplication of the two numbers:" + cal.Mul().ToString();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            cal = new CalcuClass();
+            cal.Num1 = Convert.ToInt32(txtNum1.Text);
+            cal.Num2 = Convert.ToInt32(txtNum2.Text);
+            lblResult.Text = "Division of the two numbers:" + cal.Div().ToString();
+        }
+    }
+}
